0ee55c4f7718c853d35500dabf4e006eacd08d4e
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-05-12 13:30:43 +0530

    initial version

---
+++ src/components.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from 'intermix-design-system';
+
+import { defineCustomElements, applyPolyfills } from 'intermix-design-system/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const CryptoRefresher = /*@__PURE__*/createReactComponent<JSX.CryptoRefresher, HTMLCryptoRefresherElement>('crypto-refresher');
+export const CryptoTable = /*@__PURE__*/createReactComponent<JSX.CryptoTable, HTMLCryptoTableElement>('crypto-table');
+export const CryptoView = /*@__PURE__*/createReactComponent<JSX.CryptoView, HTMLCryptoViewElement>('crypto-view');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const SimpleConfig = /*@__PURE__*/createReactComponent<JSX.SimpleConfig, HTMLSimpleConfigElement>('simple-config');
